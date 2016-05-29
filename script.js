4852028dfefff5028b866fc519df71e13b3559b8
JagArAnders <thomas.schmidt@hotmail.se>
2016-05-29 19:59:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,44 +2,156 @@
 function init(x, y){
     
 }
-
-var rectangle = {x: 0, y: 0};
+var level = 0;
+var walls1 = [0, 45, 90, 135, 180, 225, 270, 315];
+var walls2 = [45, 90, 135, 180, 225, 270, 315, 360];
+var rectangle = {x: 0, y: 0, x2: 0, y2: 0};
+var x=0;
+var y=0;
 var xx = 0;
 var yy = 0;
 
+
 function start()
 {
+    level++;
     InitMaze();
+    debugger
     rectangle.x = totalWidth-30;
     rectangle.y = totalHeight-30;
+    rectangle.x2 = totalWidth-12;
+    rectangle.y2 = totalHeight-12;
+    update();
+    
+    
 }
 
 
 function update()
 {
     debugger
+    drawMaze();
     RectangleRoom(); 
-    var rectangleLocation = room[xx][yy];
-    console.log(room[xx][yy]);
-    if (keyboard.left) {
-        rectangle.x-=3;
-        ctx.clearRect(rectangle.x+3, rectangle.y, 21, 21);
+    RectangleRoom2();
+    if (keyboard.left) {Left(); ctx.clearRect(rectangle.x, rectangle.y, 20, 20);}
+    if (keyboard.right) {Right();ctx.clearRect(rectangle.x, rectangle.y, 20, 20);}
+    if (keyboard.up) {Up();ctx.clearRect(rectangle.x, rectangle.y, 20, 20);}
+    if (keyboard.down) {Down();ctx.clearRect(rectangle.x, rectangle.y, 20, 20);}
+    rectangle(rectangle.x, rectangle.y, 20, 20, "blue");
+    rectangle(0,0,45,45,"green");
+    rectangle(rectangle.x,rectangle.y,2,2,"red")
+    rectangle(rectangle.x2,rectangle.y2,2,2,"red")
+    if (rectangle.x < 30 && rectangle.y < 30){
+        UWIN();
         
-        }
+    }
     
-    if (keyboard.right) {rectangle.x+=3;ctx.clearRect(rectangle.x-3, rectangle.y, 21, 21);}
-    if (keyboard.up) {rectangle.y-=3;ctx.clearRect(rectangle.x, rectangle.y+3, 21, 21);}
-    if (keyboard.down) {rectangle.y+=3;ctx.clearRect(rectangle.x, rectangle.y-3, 21, 21);}
-    
-    if (rectangle.x==0){rectangle.x=0}
-    if (rectangle.x==canvas.width){rectangle.x=canvas.width-20;}
-    
-    drawMaze();    
-    rectangle(rectangle.x, rectangle.y, 20, 20, "blue");  
-    
-    
+   
 
 }
+function UWIN(){
+    stopUpdate();
+    clearScreen();
+    text(10,100,20,"Congratz, beat level:"+level, "gold")
+}
+function RectangleRoom(){
+    var t = [0, 45, 90, 135, 180, 225, 270, 315];
+    var Rx = Math.floor(((rectangle.x)/45))*45;
+    var Ry = Math.floor(((rectangle.y)/45))*45;
+    if (Rx == -1){Rx=315;}
+    if (Ry == -1){Ry=315;}
+    x = t.indexOf(Rx);
+    y = t.indexOf(Ry);
+}
+    
+function RectangleRoom2(){
+    var t = [0, 45, 90, 135, 180, 225, 270, 315];
+    var Rx = Math.floor(((rectangle.x2)/45))*45;
+    var Ry = Math.floor(((rectangle.y2)/45))*45;
+    if (Rx == -1){Rx=315;}
+    if (Ry == -1){Ry=315;}
+    xx = t.indexOf(Rx);
+    yy = t.indexOf(Ry);
+    
+}
+
+function Left(){
+    if (room[x][y].w == false && walls1.indexOf((rectangle.x-3)) != -1){
+        rectangle.x-=2;
+        rectangle.x2-=2;
+    }
+    else if (room[x][y].w == false && walls1.indexOf((rectangle.x-2)) != -1){
+        rectangle.x--;
+        rectangle.x2--;
+    }
+    else if (room[x][y].w == false && walls1.indexOf((rectangle.x-1)) != -1 ||
+             room[xx][yy].w == false && walls1.indexOf((rectangle.x2-19)) != -1 ||
+             room[x-1][y].s == false && room[x][y] != room[x][yy] && rectangle.x-1==x*45)
+    {
+                
+                rectangle.x-=0;
+                rectangle.x2-=0;
+    }
+  
+    else {rectangle.x-=3;rectangle.x2-=3;}
+}
+
+function Right(){
+    if (room[x][y].e == false && walls2.indexOf((rectangle.x+23)) != -1){
+        rectangle.x+=2;
+        rectangle.x2+=2;
+
+    }
+    else if (room[x][y].e == false && walls2.indexOf((rectangle.x+22)) != -1){
+        rectangle.x++;
+        rectangle.x2++;
+    }
+    else if (room[x][y].e == false && walls2.indexOf((rectangle.x+21)) != -1||
+             room[xx][yy].e == false && walls2.indexOf((rectangle.x2+3)) != -1 ||
+             room[x+1][y].s == false && room[x][y] != room[x][yy] && rectangle.x+21==(x+1)*45 ){
+        rectangle.x+=0;
+        rectangle.x2+=0;
+    }
+    else {rectangle.x+=3;rectangle.x2+=3;}
+}
+
+function Up(){
+    if (room[x][y].n == false && walls1.indexOf((rectangle.y-3)) != -1){
+        rectangle.y-=2;
+        rectangle.y2-=2;
+    }
+    else if (room[x][y].n == false && walls1.indexOf((rectangle.y-2)) != -1){
+        rectangle.y--;
+        rectangle.y2--;
+    }
+    else if (room[x][y].n == false && walls1.indexOf((rectangle.y-1)) != -1 ||
+             room[xx][yy].n == false && walls1.indexOf((rectangle.y2-19)) != -1||
+        room[x][y-1].e == false && room[x][y] != room[xx][y] && rectangle.y-1==y*45){
+        rectangle.y-=0;
+        rectangle.y2-=0;
+    }
+    else {rectangle.y-=3;rectangle.y2-=3;}
+}
+
+function Down(){
+    if (room[x][y].s == false && walls2.indexOf((rectangle.y+23)) != -1){
+        rectangle.y+=2;
+        rectangle.y2+=2;
+    }
+    else if (room[x][y].s == false && walls2.indexOf((rectangle.y+22)) != -1){
+        rectangle.y++;
+        rectangle.y2++;
+    }
+    else if (room[x][y].s == false && walls2.indexOf((rectangle.y+21)) != -1 ||
+             room[xx][yy].s == false && walls2.indexOf((rectangle.y2+3)) != -1 ||
+             room[x][y+1].e == false && room[x][y] != room[xx][y] && rectangle.y+21==(y+1)*45){
+        rectangle.y+=0;
+        rectangle.y2+=0;
+    }
+    else {rectangle.y+=3;rectangle.y2+=3;}
+}
+
+
 var room = [];
 
 function InitMaze(){
@@ -57,7 +169,7 @@ maze();
 }
 
 function maze(){
-   debugger;
+   
 if(Math.random() > 0.5) {
  // alternativ 1A
         var a = Math.floor(Math.random()*8);
